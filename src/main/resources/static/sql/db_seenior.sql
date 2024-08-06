47c21a2b6e9152112c3c94535c23338299ba6b82
lljungho <lljungho@naver.com>
2024-08-06 15:05:01 +0900

    240806 Mod sign form fragment script

--- src/main/resources/static/sql/db_seenior.sql
+++ src/main/resources/static/sql/db_seenior.sql
@@ -11,9 +11,9 @@ CREATE TABLE ADMIN_ACCOUNT (
 	PW 						VARCHAR(200) NOT NULL COMMENT "관리자 비밀번호",							-- 관리자 비밀번호
 	NAME	 				VARCHAR(50) NOT NULL COMMENT "관리자 이름",								-- 관리자 이름
 	PHONE 					VARCHAR(50) NOT NULL COMMENT "관리자 연락처",								-- 관리자 연락처
-	DEPARTMENT 				VARCHAR(50) NOT NULL COMMENT "관리자 부서",								-- 관리자 부서
-	LEVEL 					VARCHAR(50) NOT NULL COMMENT "관리자 직급",								-- 관리자 직급
-	POSITION 				VARCHAR(50) NOT NULL COMMENT "관리자 직책",								-- 관리자 직책
+	DEPARTMENT 				VARCHAR(50) COMMENT "관리자 부서",								-- 관리자 부서
+	LEVEL 					VARCHAR(50) COMMENT "관리자 직급",								-- 관리자 직급
+	POSITION 				VARCHAR(50) COMMENT "관리자 직책",								-- 관리자 직책
 	AUTHORITY_ROLE 			VARCHAR(50) DEFAULT "NOT_APPROVED" COMMENT "관리자 인가 권한명", 			-- 관리자 인가 권한명
 	ISACCOUNTNONEXPIRED 	TINYINT DEFAULT 1 COMMENT "관리자 계정 만료 유무(만료X = 1, 만료 = 0)",			-- 관리자 계정 만료 유무(만료X = 1, 만료 = 0)
 	ISACCOUNTNONLOCKED 		TINYINT DEFAULT 1 COMMENT "관리자 계정 잠김 유무(잠김X = 1, 잠김 = 0)",			-- 관리자 계정 잠김 유무(잠김X = 1, 잠김 = 0)
