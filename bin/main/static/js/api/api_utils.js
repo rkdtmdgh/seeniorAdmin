4ac17c048dc2d044f866f5a6ea1c4188ddfa3a78
lljungho@naver.com <lljungho@naver.com>
2024-08-28 10:48:29 +0900

    240828 10:40 Mod utils.js

--- bin/main/static/js/api/api_utils.js
+++ bin/main/static/js/api/api_utils.js
@@ -1,6 +1,6 @@
 // 이메일 중복 검사 
 function usedEmailCheck(email) {
-	logger.info('usedEmailChekc:', email);
+	logger.info('usedEmailChekc():', email);
 	return $.ajax({
 		url: '/account/is_account',
 		method: 'GET',
@@ -9,11 +9,34 @@ function usedEmailCheck(email) {
 		},
 	})
 	.then(response => {
-		logger.info('Ajax usedEmailCheck result:', response);
+		logger.info('usedEmailCheck() result:', response);
 		return response;
 	})
 	.catch(error => {
 		logger.error('Error during email check:', error);
+		throw error;
+	});
+}
+
+// 질환 / 질병명 중복 검사
+function usedDiseaseCheck(dc_no, d_name) {
+	logger.info('usedDiseaseCheck() dc_no:', dc_no);
+	logger.info('usedDiseaseCheck() d_name:', d_name);
+	
+	return $.ajax({
+		url: '/disease/is_disease',
+		method: 'GET',
+		data: {
+			dc_no: dc_no,
+			d_name: d_name,
+		},
+	})
+	.then(response => {
+		logger.info('usedDiseaseCheck() result:', response);
+		return response;
+	})
+	.catch(error => {
+		logger.error('Error during disease check:', error);
 		throw error;
 	});
 }
