52f1332ce10c0676fbab59cbb274e39836b52d26
llsssyy <souldkdl93@gamil.com>
2024-09-01 02:18:06 +0900

    240901 02:17 recipe service modify

--- src/main/java/com/see_nior/seeniorAdmin/recipe/RecipeService.java
+++ src/main/java/com/see_nior/seeniorAdmin/recipe/RecipeService.java
@@ -50,110 +50,121 @@ public class RecipeService {
 			mergeJsonNodes(jsonRecipeData, json2Node);
 			
 			// JSON 데이터를 JsonNode로 파싱
-			JsonNode cookRcp01Node = jsonRecipeData.path("COOKRCP01");
-			JsonNode rowNode = cookRcp01Node.path("row");
+			JsonNode rowNode = jsonRecipeData.path("COOKRCP01").path("row");
 			
+			// rowNode를 배열 형식으로 잘 가지고 왔다면
 			if (rowNode.isArray()) {
+				
+				// RecipeDto들을 담을 리스트 선언
 				List<RecipeDto> recipes = new ArrayList<>();
 				
+				// rowNode 배열의 항목들을 JsonNode형의 row로 할당
 				for (JsonNode row : rowNode) {
 					
-					if (row.isObject()) {
-						ObjectNode objectNode = (ObjectNode) row;
-						Iterator<Entry<String, JsonNode>> fields = objectNode.fields();
+					// JsonNode는 수정할 수 없으므로 ObjectNode형으로 형변환
+					ObjectNode objectNode = (ObjectNode) row;
+					
+					// objectNode의 키와 밸류값을 반복하여 뽑기
+					Iterator<Entry<String, JsonNode>> fields = objectNode.fields();
+					
+					// objectNode의 키값을 소문자로 변환한 modifiedNode 생성
+					ObjectNode modifiedNode = objectMapper.createObjectNode();
+					
+					// fields의 길이만큼 objectNode의 키값 소문자로 변경하는 로직
+					while(fields.hasNext()) {
+						// fields에서 한개의 데이터를 키, 밸류로 지정
+						Entry<String, JsonNode> field = fields.next();
+						// field 에서 키값을 뽑아 소문자로 변경
+						String lowerCaseFieldName = field.getKey().toLowerCase();
+						// field에서 밸류값 기존 그대로 가져오기
+						JsonNode fieldValue = field.getValue();
+						// 소문자로 변경된 키값과 기존의 밸류값을 modifiedNode에 할당
+						modifiedNode.set(lowerCaseFieldName, fieldValue);
 						
-						ObjectNode modifiedNode = objectMapper.createObjectNode();
-						while(fields.hasNext()) {
-							Entry<String, JsonNode> field = fields.next();
-							String lowerCaseFieldName = field.getKey().toLowerCase();
-							JsonNode fieldValue = field.getValue();
-							modifiedNode.set(lowerCaseFieldName, fieldValue);
-							
-						}
+					}
+					
+					// modifiedNode의 데이터들을 RecipeDto형으로 변환
+					RecipeDto recipeDto = objectMapper.treeToValue(modifiedNode, RecipeDto.class);
+					
+					// recipes 리스트에 recipeDto 추가하기
+					recipes.add(recipeDto);
 						
-						RecipeDto recipeDto = objectMapper.treeToValue(modifiedNode, RecipeDto.class);
-						recipes.add(recipeDto);
-						
-					} 
 				}
 				
+				// DB에 recipes의 레시피들 insert
 				for (RecipeDto recipeDto : recipes) {
 					
 					recipeMapper.insertApiRecipeData(recipeDto);
 					
 				}
-				
-				
+			
+			// rowNode를 가지고 오지 못 했다면
 			} else {
 				log.info("Expected 'row' to be an array but found something else.");
 				
 			}
 			
-			
 		} catch (Exception e) {
 			e.printStackTrace();
 			
 		}
-		
 			
 	}
 
-	private void mergeJsonNodes(ObjectNode target, JsonNode source) {
+	// API 호출 항목 갯수 제한 때문에 전체 API 데이터를 모두 호출해 합치는 메서드
+	private void mergeJsonNodes(ObjectNode target, JsonNode jsonData) {
 		log.info("mergeJsonNodes()");
 		
-		if (source.isObject()) {
-			
-			Iterator<Entry<String, JsonNode>> fields = source.fields();
+			// jsonData의 키와 밸류값을 반복하여 뽑기
+			Iterator<Entry<String, JsonNode>> fields = jsonData.fields();
 			
 			while (fields.hasNext()) {
+				// fields에서 한개의 데이터를 키, 밸류로 지정
 				Entry<String, JsonNode> field = fields.next();
-				String fieldName = field.getKey();
+				// field에서 키값 뽑기
+				String fieldKey = field.getKey();
+				// field에서 밸류값 뽑기
 				JsonNode fieldValue = field.getValue();
 				
+				// fieldValue가 JSON객체인지 확인
 				if (fieldValue.isObject()) {
 					
-					ObjectNode subNode = (ObjectNode) target.get(fieldName);
+					// target객체에서 field fieldKey에 해당하는 값 가져와 subNode로 할당
+					ObjectNode subNode = (ObjectNode) target.get(fieldKey);
 					
+					// subNode가 null일 경우 새로운 ObjectNode 생성하여 fieldKey할당.
+					// 첫번째 객체일때 빈 fieldKey 초기화하기 위함.
 					if (subNode == null) {
 						subNode = objectMapper.createObjectNode();
-						target.set(fieldName, subNode);
+						target.set(fieldKey, subNode);
 						
 					}
+					
+					// subNode와 현재의 fieldValue 병합. 재귀적으로 호출하므로 중첩된 객체도 병합 가능
 					mergeJsonNodes(subNode, fieldValue);
 					
+				// fieldValue가 배열일 경우
 				} else if (fieldValue.isArray()) {
 					
-					ArrayNode arrayNode = (ArrayNode) target.withArray(fieldName);
+					// target에서 fieldKey에 대응하는 배열을 가져오거나, 없으면 새로 생성하여 arrayNode에 할당.
+					ArrayNode arrayNode = (ArrayNode) target.withArray(fieldKey);
 					
-					for (JsonNode element : fieldValue) {
-						arrayNode.add(element);
+					// fieldValue를 순회하며 fieldValue의 모든 요소가 arrayNode에 할당
+					for (JsonNode fieldElement : fieldValue) {
+						
+						// fieldElement의 각 요소들을 target의 배열 필드에 결합하여, 기존의 배열의 내용이 유지되며 새로운 요소들이 병합됨.
+						arrayNode.add(fieldElement);
 						
 					}
 					
+				// 배열이나 객체가 아닌 단순 데이터 형식일 시, target에 그대로 할당
 				} else {
-					target.set(fieldName, fieldValue);
+					target.set(fieldKey, fieldValue);
 					
 				}
 				
-				
 			}
 			
-		} 
-//		else if (source.isArray()) {
-//			
-//			ArrayNode arrayNode = objectMapper.createArrayNode();
-//			if (target.has("row")) {
-//				arrayNode = (ArrayNode) target.get("row");
-//				
-//			}
-//			for (JsonNode element : source) {
-//				arrayNode.add(element);
-//				
-//			}
-//			target.set("row", arrayNode);
-//			
-//			}
-			
-		}
+	}
 		
-	} 
+} 
