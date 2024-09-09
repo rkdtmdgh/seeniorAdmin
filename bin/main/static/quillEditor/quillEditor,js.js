ed135bad84ef7ae9323a83e2143ec9cacf88620f
lljungho@naver.com <lljungho@naver.com>
2024-09-09 17:13:32 +0900

    240909 17:13 Mod quillEditor.js

--- bin/main/static/quillEditor/quillEditor,js.js
+++ bin/main/static/quillEditor/quillEditor,js.js
@@ -25,4 +25,65 @@ $(document).ready(function() {
 		placeholder: '내용을 입력하세요.',
 		theme: 'snow',
 	});
+	
+	// 이미지 버튼 이벤트 처리
+	quill.getModule('toolbar').addHandler('image', function () {
+        selectLocalImage();
+    });
+
+	function selectLocalImage() {
+		const fileInput = document.createElement('input');
+		fileInput.setAttribute('type', 'file');
+		fileInput.accept = "image/*";
+
+		fileInput.click();
+
+		fileInput.addEventListener("change", function () {
+			if (quill) {
+				uploadImage(fileInput.files[0]);
+			}
+		});
+	}
+
+	// 이미지 드롭 이벤트 핸들러 등록
+	quill.root.addEventListener('drop', function (event) {
+		event.preventDefault();
+		if (quill) {
+			const file = event.dataTransfer.files[0];
+			uploadImage(file);
+		}
+	});
+
+	// 이미지 업로드 함수
+	function uploadImage(file) {
+		if (!quill) {
+			return;
+		}
+		
+		const formData = new FormData();
+		formData.append('uploadFile', file);
+
+		$.ajax({
+			type: 'post',
+			enctype: 'multipart/form-data',
+			url: '/quillEditor/upimg_ok.asp',
+			data: formData,
+			processData: false,
+			contentType: false,
+			dataType: 'json',
+			success: function (data) {
+				if (data.errorNum === 0) {
+					// 이미지 삽입을 제어
+					const range = quill.getSelection();
+					quill.clipboard.dangerouslyPasteHTML(range ? range.index : 0, `<img src="${data.imageUrl}" alt="Image" />`);
+				} else {
+					alert(data.errorMessage);
+				}
+			},
+			error: function (err) {
+				console.log('ERROR!! ::');
+				console.log(err);
+			}
+		});
+	}
 });
