b84f80aba60fb23af7221fb338d9f295febca84b
lljungho@naver.com <lljungho@naver.com>
2024-10-22 11:57:30 +0900

    241022 11:56 Add user account is_blocked api

--- src/main/resources/static/js/modal.js
+++ src/main/resources/static/js/modal.js
@@ -1,19 +1,30 @@
-// 모달창 제어
+// 모달 열기
 function openModal(formName, contentFunction) {
 	const $form = $(`form[name="${formName}"]`);
-	const $dimmed = $('<div class="dimmed_wrap" onclick="modalClose();">'); // 딤드 생성
 	const infoForm = modalContent(formName, contentFunction); // info HTML 가져오기
 	const $modalContent = $(`
-        <div class="modal_wrap sc">
-            <div class="modal_container">
-            	<div class="modal_info content_edit_table">
-	                ${infoForm}
+        <div class="fixed_wrap">
+        	<div class="dimmed_wrap" onclick="closeModal();"></div>
+	        <div class="modal_wrap sc">
+	            <div class="modal_container">
+	            	<div class="modal_info content_edit_table">
+		                ${infoForm}
+		            </div>
 	            </div>
-            </div>
+	        </div>
         </div>
 	`);
 	 
-	$form.append($dimmed, $modalContent); 
+	$form.append($modalContent); // 모달 추가
+	$('.dimmed_wrap').fadeIn(100);
+	$('.modal_container').fadeIn(300).css('margin-top', 0);
+}
+
+// 모달 닫기
+function closeModal() {
+    $('.fixed_wrap').fadeOut(100, function() {
+        $(this).remove();
+    });
 }
 
 // 회원 계정 상태 변경 폼
@@ -22,18 +33,21 @@ function modalContent(formName, contentFunction) {
 		case 'putUserAccountBlockModify':
 			return `
 				<p class="modal_title bold">회원 계정 정지 사유</p>
+				
 				<div class="modal_info_list">
 					<textarea name="u_blocked_reason" class="border_textarea" placeholder="계정 정지 사유룰 입력하세요"
 						oninput="setTextLimit(this, 'short')"
 					></textarea>
+					
 					<div id="text_limit">
         				<span id="current_size">0</span> / <span id="max_size">250 byte</span>
         			</div>
+        			
+        			<div class="btn_list">
+	                    <div onclick="closeModal();" class="btns cancel">닫기</div>
+	                    <div onclick="putUserAccountBlockModify('${formName}', false)" class="btns">계정 정지</div>
+	                </div>
 				</div>
-                <div class="btn_list">
-                    <div onclick="modalClose();" class="btns cancel">닫기</div>
-                    <div onclick="putUserAccountBlockModify('${formName}')" class="btns">계정 정지</div>
-                </div>
 			`;
 	}
 }
