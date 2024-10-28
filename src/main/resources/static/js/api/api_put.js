bdc7c7d34bd86ada046956259fe0dd39feb875f6
lljungho@naver.com <lljungho@naver.com>
2024-10-28 14:29:58 +0900

    241028 14:30 Add qna delete api

--- src/main/resources/static/js/api/api_put.js
+++ src/main/resources/static/js/api/api_put.js
@@ -560,7 +560,7 @@ async function putAnswerModify(formName) {
 	}
 	
 	const formData = new FormData(form);
-	const successMessage = `"${form.a_id.value}" 님의 닫변이 수정되었습니다.`;
+	const successMessage = `"${form.a_id.value}" 님의 답변이 수정되었습니다.`;
 	const errorMessage = `"${form.a_id.value}" 님의 답변 수정에 실패했습니다. 다시 시도해 주세요.\n문제가 지속될 경우 관리자에게 문의해 주세요.`;
 	
 	await putIntegSubmit(
