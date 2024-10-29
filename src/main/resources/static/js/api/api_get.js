dd8b9b50f725f7020084cfcfe5ea441191a0f7c0
lljungho@naver.com <lljungho@naver.com>
2024-10-29 12:25:39 +0900

    241029 12:25 qna delete check

--- src/main/resources/static/js/api/api_get.js
+++ src/main/resources/static/js/api/api_get.js
@@ -904,7 +904,7 @@ function getSortList(event, dbTable, sortValue) {
 
 // sortType에 따른 sort getList() 요청 커맨드 설정
 function mapSortListApiObject(dbTable, sortType) {
-	// dbTable과 sortType 조합에 따른 URL 매핑
+	// dbTable과 sortType 조합에 따른 커맨드 매핑
 	const apiUrlMap = { // 0 = 기본값, 1 = 검색, 2 = 카테고리선택
 		'admin_account': { // 관리자 계정 관리 페이지
 			0: '/account/list/get_admin_list',
@@ -1145,7 +1145,6 @@ function mapCategorylistObject(ele) {
 	let getCateSelectApiUrl = null; // 분류별 리스트 요청 api
 	let getSelectMaxIdxApiUrl = null; // 분류 또는 분류에 속한 데이터에 순번 입력이 필요한 경우 max값 요청 api
 	let getListDtos = null; // 객체명
-	let dbtable = null; // db table명
 	let infoNo = null; // 분류 no 값
 	let infoName = null; // 분류 no 값을 가져오기 위한 객체명
 	let soltValue = null; // 정렬 기준값
@@ -1154,8 +1153,7 @@ function mapCategorylistObject(ele) {
 	switch(ele) {
 		case 'd_category_no': // 질병군별 분류 리스트(분류별 관리o)
 			getCateSelectApiUrl = '/disease/cate_info/get_category_list_select';
-			getListDtos = 'diseaseCategoryDtos';	
-			dbtable = 'disease_category';		
+			getListDtos = 'diseaseCategoryDtos';			
 			infoNo = 'dc_no';
 			infoName = 'dc_name';
 			soltValue = 'd_no';
@@ -1164,7 +1162,6 @@ function mapCategorylistObject(ele) {
 		case 'rcp_pat2': // 음식 종류별 분류 리스트(분류별 관리x)
 			getCateSelectApiUrl = '/recipe/info/get_type_list_select';
 			getListDtos = 'recipeTypeDtos';
-			dbtable = 'recipe';	
 			infoNo = 'rcp_pat2';
 			infoName = 'rcp_pat2';
 			soltValue = 'rcp_pat2';
@@ -1181,7 +1178,7 @@ function mapCategorylistObject(ele) {
 		case 'ad_category_no': // 위치별 분류 리스트(분류별 관리o)
 			getCateSelectApiUrl = '/advertisement/cate_info/get_category_list_select';
 			getSelectMaxIdxApiUrl = '/advertisement/info/create_category_select';
-			getListDtos = 'advertisementCategoryDto';
+			getListDtos = 'advertisementCategoryDtos';
 			infoNo = 'ac_no';
 			infoName = 'ac_name';
 			soltValue = 'ad_no';
