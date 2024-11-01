c82134e56d76cb542366f9e8053b3024b5229791
lljungho@naver.com <lljungho@naver.com>
2024-11-01 14:13:02 +0900

    241101 14:12 Add notice create

--- bin/main/static/js/api/api_post.js
+++ bin/main/static/js/api/api_post.js
@@ -297,15 +297,30 @@ async function postVideoCreate(formName) {
 }
 
 // 공지사항 등록
-async function postNoticeCreate(formName) {
+async function postQnaNoticeCreate(formName) {
 	const form = document.forms[formName];
-
-	const formData = new FormData(form);
-	formData.set('n_body', quill.root.innerHTML);
-
+	
+	input = form.n_title;
+	if(!validateEmpty(input, '제목을', true)) {
+		input.focus();
+		return false;
+	}
+	
+	if(!validateQuill(quill)) { // 내용 유효성 및 비속어 검사
+		quill.focus();
+		return false;
+	}
+	
 	const successMessage = '공지사항이 등록되었습니다.';
 	const errorMessage = '공지사항 등록에 실패했습니다. 다시 시도해 주세요.\n문제가 지속될 경우 관리자에게 문의해 주세요.';
 	
+	const formData = new FormData(form);
+	formData.set('bpn_body', quill.root.innerHTML); // quill 에디터 내용
+	
+	// 이미지 파일 리사이즈 및 압축하여 formData에 담기 (선택된 이미지 요소가 없을 시 빈 파일 객체가 담김)
+	const $imgTags = $(quill.root).find('img'); // 모든 이미지 태그 탐색
+	await addImagesToFormData($imgTags, formData);
+
 	await postIntegSubmit(
 		'/notice/info/create_confirm', 
 		formData, 
