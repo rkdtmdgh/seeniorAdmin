4f3aafd0f32c39b11fb9d08585ae74aad25070ad
DESKTOP-QAICPE9\c <rkdtmdgh3@naver.com>
2024-11-01 12:21:17 +0900

    20241101 12:21 fix qna modify_category_form

--- src/main/java/com/see_nior/seeniorAdmin/qna/QnaController.java
+++ src/main/java/com/see_nior/seeniorAdmin/qna/QnaController.java
@@ -450,19 +450,7 @@ public class QnaController {
 	public Object qnaTest() {
 		log.info("test()");
 		
-		Map<String, Object> searchQnaNoticeList = 
-				qnaService.searchQnaNoticePagingList("a_id", "admin", "bqn_no", "desc", 1);
-		
-		Map<String, Object> searchQnaNoticeListPageNum = 
-				qnaService.searchQnaNoticeListPageNum("bqn_no", "desc", 1);
-		
-		searchQnaNoticeList.put("searchQnaNoticeListPageNum", searchQnaNoticeListPageNum);
-		searchQnaNoticeList.put("sortValue", "bqn_no");
-		searchQnaNoticeList.put("order", "desc");
-		searchQnaNoticeList.put("searchPart", "a_id");
-		searchQnaNoticeList.put("searchString", "admin");
-		
-		return searchQnaNoticeList;
+		return qnaService.getQnaCategoryDtoByNo(1);
 		
 	}
 	
