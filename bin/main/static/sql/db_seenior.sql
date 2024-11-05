6be0fd26b6d17f9e8b70f4a99600f6db65813d05
lljungho@naver.com <lljungho@naver.com>
2024-11-05 15:11:11 +0900

    241105 15:10 Add report

--- bin/main/static/sql/db_seenior.sql
+++ bin/main/static/sql/db_seenior.sql
@@ -298,6 +298,7 @@ CREATE TABLE BOARD_POSTS (
 	BP_TITLE			VARCHAR(200) NOT NULL COMMENT "게시물 제목", 										-- 게시물 제목
 	BP_BODY				TEXT NOT NULL COMMENT "게시물 본문", 												-- 게시물 본문
 	BP_WRITER_NO		INT NOT NULL COMMENT "게시물 작성자 NO(USER_ACCOUNT TABLE PK)", 					-- 게시물 작성자 NO(USER_ACCOUNT TABLE PK)
+	BP_WRITER_ID		VARCHAR(100) NOT NULL COMMENT "게시물 작성자 ID(USER_ACCOUNT or ADMIN_ACCOUNT UNIQUE)", 		-- 게시물 작성자 ID(USER_ACCOUNT or ADMIN_ACCOUNT UNIQUE)
     BP_ACCOUNT			VARCHAR(20) NOT NULL COMMENT "게시물 작성자 유형(admin or user)",					-- 게시물 작성자 유형(admin or user)
     BP_REPORT_STATE		TINYINT DEFAULT 1 COMMENT "게시물 신고 진행 상태(기본값 = 1, 처리중 = 2, 처리 완료 = 0)",	-- 게시물 신고 진행 상태(기본값 = 1, 처리중 = 2, 처리 완료 = 0)
 	BP_VIEW_CNT			INT DEFAULT 0 COMMENT "게시물 조회수", 												-- 게시물 조회수 
@@ -373,7 +374,6 @@ CREATE TABLE BOARD_QNA (
     PRIMARY KEY(BQ_NO)
 );
 
-SELECT * FROM BOARD_QNA where bq_is_deleted = 1 and bq_answer_no is null;
 SELECT * FROM BOARD_QNA;
 SHOW INDEX FROM BOARD_QNA;
 DROP TABLE BOARD_QNA;
