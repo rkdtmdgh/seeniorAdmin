d7b7c06a32cfab47936644b3ff080a1adc2f066a
lljungho@naver.com <lljungho@naver.com>
2024-11-08 14:38:33 +0900

    check

--- src/main/resources/static/js/api/api_get.js
+++ src/main/resources/static/js/api/api_get.js
@@ -888,10 +888,10 @@ function generateTableList(apiUrl, data, getListCnt, listIndex, page) {
 		                </a>
 		            </td>
 		            <td>
-		                <a href="/qna/noti_info/modify_notice_form?bqn_no=${data.bqn_no}" class="table_info">${data.bqn_view_cnt}</a>
+		                <a href="/account/list/admin_modify_form?a_no=${data.adminAccountDto.a_no}" class="table_info">${data.adminAccountDto.a_name}</a>
 		            </td>
 		            <td>
-		                <a href="/account/list/admin_modify_form?a_no=${data.adminAccountDto.a_no}" class="table_info">${data.adminAccountDto.a_name}</a>
+		                <a href="/qna/noti_info/modify_notice_form?bqn_no=${data.bqn_no}" class="table_info">${data.bqn_view_cnt}</a>
 		            </td>
 		            <td class="va_m">
 		                <a href="/qna/noti_info/modify_notice_form?bqn_no=${data.bqn_no}" class="flex_area">
@@ -1023,6 +1023,9 @@ function generateTableList(apiUrl, data, getListCnt, listIndex, page) {
 		                		${data.bp_report_state === 2 ? '처리중' : data.bp_report_state === 0 ? '처리완료' : '정상'}
 		                	</span>
 		                </a>
+		            </td>
+		            <td>
+		                <a href="/board/info/modify_form?infoNo=${data.bp_category_no}&bp_no=${data.bp_no}" class="table_info">${setFormatDate(data.bp_reg_date)}</a>
 		            </td>
 		            <td>
 		                <a href="/board/info/modify_form?infoNo=${data.bp_category_no}&bp_no=${data.bp_no}" class="table_info">${setFormatDate(data.bp_mod_date)}</a>
