5c6efb12b697c8464e48a2246b6515ab94788b61
lljungho@naver.com <lljungho@naver.com>
2024-11-15 11:26:56 +0900

    241115 11:26 Mod reply layout

--- bin/main/static/js/api/api_get.js
+++ bin/main/static/js/api/api_get.js
@@ -1032,8 +1032,8 @@ function generateTableList(apiUrl, data, getListCnt, listIndex, page) {
 		            </td>
 		            <td class="va_m">
 		                <a href="/board/info/modify_form?infoNo=${data.bp_category_no}&bp_no=${data.bp_no}" class="flex_area">
-		                	<span class="state ${data.bp_report_state === 1 ? '' : 'on'}">
-		                		${data.bp_report_state === 1 ? '정상' : '블럭'}
+		                	<span class="state ${data.bp_report_state === true ? 'off' : ''}">
+		                		${data.bp_report_state === true ? '정지' : '정상'}
 		                	</span>
 		                </a>
 		            </td>
