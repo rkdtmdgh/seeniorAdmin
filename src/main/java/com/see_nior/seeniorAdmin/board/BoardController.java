8f6bcc17279dc493e212adbe99bded6a7aaf2277
ComposerL <theboygirl@naver.com>
2025-03-20 03:51:41 +0900

    20250320 am03:50 /board notice fixing

--- src/main/java/com/see_nior/seeniorAdmin/board/BoardController.java
+++ src/main/java/com/see_nior/seeniorAdmin/board/BoardController.java
@@ -392,14 +392,15 @@ public class BoardController {
 	//작성한 공지 게시물 등록 요청(board_notice)
 	@PostMapping("/noti_info/create_notice_confirm")
 	@ResponseBody
-	public boolean createNoticeConfirm(@RequestParam("files") List<MultipartFile> files, 
+	public boolean createNoticeConfirm(@RequestParam(value = "files" , required = false) List<MultipartFile> files, 
 									BoardNoticePostsDto boardNoticePostsDto) {
 		log.info("createNoticeConfirm()");
 		
 		log.info("files: {}",files.size());
 		log.info("bn_category_no: {}",boardNoticePostsDto.getBn_category_no());
 		log.info("bn_writer_no: {}",boardNoticePostsDto.getBn_writer_no());
-				
+		log.info("bn_title: {}",boardNoticePostsDto.getBn_title());
+		log.info("bn_body: {}",boardNoticePostsDto.getBn_body());
 		Boolean result = false;
 		
 		//file 첨부가 되어 있는지 확인
